17179b881d5815d2c93cbed960cf32354474903d
Katerina <kathty@gmail.com>
2021-12-09 08:53:12 +0300

    Add new in section

--- source/js/main.js
+++ source/js/main.js
@@ -17,3 +17,27 @@ if(toggleButton){
   });
 }
 
+// Slider
+// eslint-disable-next-line no-undef
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 2,
+  slidesPerGroup: 2,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    renderBullet: (index, className) => `<span class="${className}">${index + 1}</span>`,
+  },
+  navigation: {
+    nextEl: '.products__button--next',
+    prevEl: '.products__button--prev',
+  },
+  breakpoints: {
+    1024: {
+      slidesPerView: 4,
+      slidesPerGroup: 4,
+    },
+  },
+});
+swiper;

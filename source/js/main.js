92c0e160a09fdd26a80ec9f8ad4be04ba2226d67
Katerina <kathty@gmail.com>
2021-12-15 09:47:39 +0300

    Add login modal

--- source/js/main.js
+++ source/js/main.js
@@ -4,8 +4,24 @@ document.body.classList.remove('no-js');
 
 const pageHeader = document.querySelector('.page-header');
 const toggleButton = pageHeader.querySelector('.page-header__toggle');
+const login = pageHeader.querySelector('.page-header__login');
 
-if(toggleButton){
+const modal = document.querySelector('.modal');
+const loginModalClose = modal.querySelector('.modal__close');
+const loginFormModal = modal.querySelector('.login-form');
+const emailInputModal = loginFormModal.querySelector('[name="email"]');
+const passwordInputModal = loginFormModal.querySelector('[name="password"]');
+// Local storage
+let isStorageSupport = true;
+let storage = '';
+
+try {
+  storage = localStorage.getItem('email');
+} catch (err) {
+  isStorageSupport = false;
+}
+
+if (toggleButton) {
   toggleButton.addEventListener('click', () => {
     pageHeader.classList.toggle('page-header--opened');
 
@@ -17,33 +33,60 @@ if(toggleButton){
   });
 }
 
-// Slider
-// eslint-disable-next-line no-undef
-const swiper = new Swiper('.swiper', {
-  slidesPerView: 2,
-  slidesPerGroup: 2,
-  spaceBetween: 30,
-  loop: true,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-    renderBullet: (index, className) => `<span class="${className}">${index + 1}</span>`,
-  },
-  navigation: {
-    nextEl: '.products__button--next',
-    prevEl: '.products__button--prev',
-  },
-  breakpoints: {
-    1024: {
-      slidesPerView: 4,
-      slidesPerGroup: 4,
-    },
-  },
+window.addEventListener('click', (evt) => {
+  if (evt.target === modal) {
+    closeLoginModal();
+  }
 });
-swiper;
 
-// Accordion
-const accordionItems = document.querySelectorAll('.accordion__item');
+const modalEscKeydownHandler = (evt) => {
+  if (evt.key === 'Escape' || evt.key === 'Esc') {
+    evt.preventDefault();
+    closeLoginModal();
+};
+if (login) {
+  login.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    openLoginModal();
+  });
+}
+
+if (loginFormModal) {
+  loginFormModal.addEventListener('submit', (evt) => {
+    if (!emailInputModal.value || !passwordInputModal.value) {
+      evt.preventDefault();
+    } else {
+      if (isStorageSupport) {
+        localStorage.setItem('email', emailInputModal.value);
+      }
+    }
+  });
+}
+
+if (loginModalClose) {
+  loginModalClose.addEventListener('click', closeLoginModal);
+}
+
+function openLoginModal() {
+  modal.classList.add('modal--show');
+  document.body.style.overflow = 'hidden';
+
+  if (storage) {
+    emailInputModal.value = storage;
+    passwordInputModal.focus();
+  } else {
+    emailInputModal.focus();
+  }
+
+  loginModalClose.addEventListener('click', closeLoginModal);
+  document.addEventListener('keydown', modalEscKeydownHandler);
+}
+
+function closeLoginModal() {
+  modal.classList.remove('modal--show');
+  document.body.style.overflow = 'auto';
+  document.removeEventListener('keydown', modalEscKeydownHandler);
+}
 
 if (accordionItems) {
   accordionItems.forEach((item) => {
